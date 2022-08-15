0820d35fcfa91b9693c931ddb752151297dd3f0f
Omar Hosny <honsyomar022@gmail.com>
2022-08-15 10:35:59 +0200

    Some Changes Added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { AuthContext } from "./context/auth-context";
+import Ingredients from "./components/Ingredients/Ingredients";
+import Auth from "./components/Auth";
 
-import Ingredients from './components/Ingredients/Ingredients';
-
-const App = props => {
+const App = (props) => {
+  const authContext = useContext(AuthContext);
+  if (!authContext.isAuth) {
+    return <Auth />;
+  }
   return <Ingredients />;
 };
 
